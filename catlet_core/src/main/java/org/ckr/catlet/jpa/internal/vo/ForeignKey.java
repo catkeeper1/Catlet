2769667b2550aaa1877b995b692d90d74a58f1be
ruoli.chen <ruoli.ri.chen@gtomato.com>
2021-02-15 18:26:02 +0800

    parse index

--- catlet_core/src/main/java/org/ckr/catlet/jpa/internal/vo/ForeignKey.java
+++ catlet_core/src/main/java/org/ckr/catlet/jpa/internal/vo/ForeignKey.java
@@ -4,17 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ForeignKey {
-    private String joinType;
 
-    private String sourceTableName = "";
+    private Boolean isMany = false;
 
-    private String targetTableName = "";
+    private Table targetTable;
 
-    private List<String> sourceColumnNames = new ArrayList<>();
+    private List<Column> sourceColumns = new ArrayList<>();
 
-    private List<String> targetColumnNames = new ArrayList<>();
+    private List<Column> targetColumns = new ArrayList<>();
 
     private Boolean optional = true;
 
+    public Boolean getMany() {
+        return isMany;
+    }
 
+    public void setMany(Boolean many) {
+        isMany = many;
+    }
+
+    public Table getTargetTable() {
+        return targetTable;
+    }
+
+    public void setTargetTable(Table targetTable) {
+        this.targetTable = targetTable;
+    }
+
+    public List<Column> getSourceColumns() {
+        return sourceColumns;
+    }
+
+    public void setSourceColumns(List<Column> sourceColumns) {
+        this.sourceColumns = sourceColumns;
+    }
+
+    public List<Column> getTargetColumns() {
+        return targetColumns;
+    }
+
+    public void setTargetColumns(List<Column> targetColumns) {
+        this.targetColumns = targetColumns;
+    }
+
+    public Boolean getOptional() {
+        return optional;
+    }
+
+    public void setOptional(Boolean optional) {
+        this.optional = optional;
+    }
 }
