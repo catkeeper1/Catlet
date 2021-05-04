3ce9dc5d965e9c033127d5aa59b0e706fc381494
catkeeper1 <catkeeper1@gmail.com>
2021-05-04 22:40:29 +0800

    upgrade plantuml lib.

--- catlet_core/src/main/java/module-info.java
+++ catlet_core/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module org.ckr.catlet {
     exports org.ckr.catlet.plantuml;
     exports org.ckr.catlet.jpa;
     requires jdk.javadoc;
-    requires plantuml;
+    requires net.sourceforge.plantuml;
     requires java.logging;
     requires hibernate.jpa;
     requires java.sql;
